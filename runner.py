acf31bdca1ffec6b804c68a0afe685fdb6a53955
Ali Pesaranghader <alipsgh@gmail.com>
2022-03-08 14:54:30 -0500

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,136 @@
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torchvision.utils import make_grid, save_image
+from torch.utils import data
+
+from learners.supervised.ann import ANN
+from learners.unsupervised.ae import AE
+from learners.unsupervised.rbm_pytorch import RBM
+import matplotlib.pyplot as plt
+
+torch.manual_seed(0)
+if torch.cuda.is_available():
+    print("Setting seed for GPU")
+    torch.cuda.manual_seed_all(0)
+
+
+def show_and_save(file_name, img):
+    if torch.cuda.is_available():
+        img = img.cpu()
+
+    np_img = np.transpose(img.numpy(), (1, 2, 0))
+
+    f = "./%s.png" % file_name
+    plt.imshow(np_img, cmap=plt.cm.binary)
+    plt.imsave(f, np_img, cmap=plt.cm.binary)
+
+
+training_set = datasets.MNIST(root='./data/mnist/', train=True, transform=transforms.ToTensor(), download=True)
+test_set = datasets.MNIST(root='./data/mnist/', train=False, transform=transforms.ToTensor())
+
+print(len(training_set), "instances exist for training.")
+print(len(test_set), "instances exist for testing.")
+
+batch_size = 64
+num_epochs = 64
+
+training_set_loader = data.DataLoader(dataset=training_set, batch_size=batch_size, shuffle=True)
+test_set_loader = data.DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False)
+
+input_dim = 28 * 28
+output_dim = 10
+
+'''
+ann = ANN(input_dim=input_dim, hidden_layers_dim=[100, 100, 100], output_dim=output_dim, activations=['relu'] * 3, optimizer='adam', learning_rate=0.001)
+print(ann)
+'''
+
+dae = AE(input_dim=input_dim, encoders_dim=[625, 529, 324, 100], encoder_activations=['relu'] * 4, optimizer='adam', learning_rate=0.001)
+
+print(dae.linear_layers)
+
+'''
+compress_dim = 500
+rbm = RBM(vis_dim=input_dim, hid_dim=compress_dim, k=15, learning_rate=0.1)
+'''
+
+for epoch in range(num_epochs):
+
+    loss_ = []
+    iteration_counter = 0
+
+    for i, (train_X, train_y) in enumerate(training_set_loader):
+
+        '''
+        train_X = train_X.view(-1, 28 * 28)
+        train_cost, train_loss = rbm.fit(train_X)
+        loss_.append(train_cost)
+        iteration_counter += 1
+
+        # if iteration_counter % 500 == 0:
+        #    print(epoch, iteration_counter, sum(loss_) / len(loss_))
+        '''
+
+        train_X = train_X.view(-1, 28 * 28)
+        train_loss = dae.fit(train_X)
+
+        iteration_counter += 1
+        if iteration_counter % 500 == 0:
+            print(epoch, iteration_counter, "{:.4f}".format(train_loss))
+
+        '''
+        train_X = train_X.view(-1, 28 * 28)
+        train_loss = ann.fit(train_X, train_y)
+
+        iteration_counter += 1
+        if iteration_counter % 500 == 0:
+
+            total, correct = 0, 0
+
+            for test_X, test_y in test_set_loader:
+
+                test_X = test_X.view(-1, 28 * 28)
+                y_ = ann.predict(test_X)
+
+                total += test_y.size(0)
+                correct += torch.eq(y_, test_y).sum()
+
+            accuracy = 100 * (correct / total)
+
+            print("Iteration: {}, loss: {:.4f}, accuracy: {:.2f}%.".format(iteration_counter, train_loss, accuracy))
+
+        print(epoch, iteration_counter, "{:.2f}".format(sum(loss_) / len(loss_)))
+        '''
+
+# show_and_save("weights_{:.1f}".format(epoch), make_grid(rbm.W.view(compress_dim, 1, 28, 28).data))
+
+for test_X, test_y in test_set_loader:
+    test_X = test_X.view(-1, 28 * 28)
+
+    show_and_save("original", make_grid(test_X.view(batch_size, 1, 28, 28)))
+
+    # test_X_ = rbm.compress(test_X)
+    test_X_ = dae.reconstruct(test_X)
+    show_and_save("generated", make_grid(test_X_.view(batch_size, 1, 28, 28).data))
+
+    test_X_ = dae.encode(test_X)
+    show_and_save("encoded", make_grid(test_X_.view(batch_size, 1, 10, 10).data))
+
+    break
+
+Ws, Wm = dae.get_weight()
+show_and_save("weights_dae", make_grid(Wm.view(100, 1, 28, 28).data))
+
+w = Ws[0]
+show_and_save("weights_dae_1", make_grid(w.cpu().view(625, 1, 28, 28).data))
+
+w = Ws[1]
+show_and_save("weights_dae_2", make_grid(w.cpu().view(529, 1, 25, 25).data))
+
+w = Ws[2]
+show_and_save("weights_dae_3", make_grid(w.cpu().view(324, 1, 23, 23).data))
+
+w = Ws[3]
+show_and_save("weights_dae_4", make_grid(w.cpu().view(100, 1, 18, 18).data))
